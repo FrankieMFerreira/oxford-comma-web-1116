bdeefe389e91a5a3bf73df8269c7946c28da7c7e
Frankie Ferreira <frankiemferreira@gmail.com>
2016-11-13 16:15:49 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length == 3
+    content = ["and ", array[2]].join
+    array.pop
+    array << content
+    array.join(", ")
+  elsif array.length > 3
+    content = ["and ", array.last].join
+    array.pop
+    array << content
+    array.join(", ")
+  end
 end
